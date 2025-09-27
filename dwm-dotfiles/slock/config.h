005b74f28bdb4e042517b9145ed7d7ec4ed20707
kuzanf3b <0differenthang@gmail.com>
2025-09-27 11:54:41 +0700

    fix messages

--- dwm-dotfiles/slock/config.h
+++ dwm-dotfiles/slock/config.h
@@ -4,13 +4,23 @@ static const char *group = "kuzan";
 
 static const char *colorname[NUMCOLS] = {
     [INIT] = "#1A1B26",   /* after initialization */
-    [INPUT] = "#C0CAF5",  /* during input */
+    [INPUT] = "#9D7CD8",  /* during input */
     [FAILED] = "#FF9E64", /* wrong password */
     [CAPS] = "#F7768E",   /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char *message = "Locked at " __DATE__ " " __TIME__;
+
+/* text color */
+static const char *text_color = "#C0CAF5";
+
+/* text size / font */
+static const char *font_name =
+    "-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1";
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
