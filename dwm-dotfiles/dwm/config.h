a0ebf2b78eb562933b6f9f9c1cb533c0f9bf99b9
Zen <0differenthanG@gmail.com>
2025-10-20 21:08:01 +0700

    new st

--- dwm-dotfiles/dwm/config.h
+++ dwm-dotfiles/dwm/config.h
@@ -98,7 +98,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[] = { "st", NULL };
+static const char *termcmd[] = { "alacritty", NULL };
 
 /* Volome Control */
 static const char *upvol[]   = { "pamixer", "--increase", "5", NULL };
