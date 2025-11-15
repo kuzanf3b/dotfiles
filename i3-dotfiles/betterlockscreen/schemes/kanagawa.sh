e9863340abde6e8b78afcfefbb83784c098a9b82
zen <0differenthanG@gmail.com>
2025-11-15 23:31:43 +0700

    add bls & picom in i3

---
+++ i3-dotfiles/betterlockscreen/schemes/kanagawa.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Kanagawa colors
+BG="#1f1f28"
+FG="#dcd7ba"
+CMT="#727169"
+CYA="#7fb4ca"
+GRN="#98bb6c"
+ORA="#ef9f76"
+PUR="#938aa9"
+RED="#ec5f67"
+YEL="#e6c384"
+
+# Lock screen specific colors
+RING="$FG"
+INSIDE="$BG"
+RING_VER="$FG"
+INSIDE_VER="$BG"
+RING_WRONG="$FG"
+INSIDE_WRONG="$RED"
+TIME="$FG"
+KEYHL="$RED"
+BSHL="$RED"
---
+++ i3-dotfiles/betterlockscreen/schemes/rose-pine.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Rose Pine Moon colors
+BG="#191724"
+FG="#e0def4"
+CMT="#6e6a86"
+CYA="#9ccfd8"
+GRN="#31748f"
+ORA="#ebbcba"
+PUR="#c4a7e7"
+RED="#eb6f92"
+YEL="#f6c177"
+
+# Lock screen specific colors
+RING="$FG"
+INSIDE="$BG"
+RING_VER="$FG"
+INSIDE_VER="$BG"
+RING_WRONG="$FG"
+INSIDE_WRONG="$RED"
+TIME="$FG"
+KEYHL="$RED"
+BSHL="$RED"
